81c74a11501272e9d65ea9c1ca6e7905d4d1f775
2022developer <104691019+2022developer@users.noreply.github.com>
2023-03-04 22:48:24 +0530

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+export var firebaseConfig = {
+    apiKey: "AIzaSyBHHe28M2-A9TSuTs8N_V3M1OZShGqYX0E",
+    authDomain: "phonicscirclepro.firebaseapp.com",
+    projectId: "phonicscirclepro",
+    storageBucket: "phonicscirclepro.appspot.com",
+    messagingSenderId: "358848774336",
+    appId: "1:358848774336:web:67adaad9b1d49f0bd3ed25",
+    measurementId: "G-4XP4PZY922"
+  };
